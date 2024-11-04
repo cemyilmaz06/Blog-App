dbc22904b409d22d7549e363cc35ce00d8fa3acf
cem <cemyilmaztech@gmail.com>
2024-11-04 23:38:00 +0300

    textfield

--- src/pages/NewBlog.jsx
+++ src/pages/NewBlog.jsx
@@ -1,49 +1,29 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import MenuItem from '@mui/material/MenuItem';
+import { MenuItem } from '@mui/material';
 
-const currencies = [
-  {
-    value: 'USD',
-    label: '$',
-  },
-  {
-    value: 'EUR',
-    label: '€',
-  },
-  {
-    value: 'BTC',
-    label: '฿',
-  },
-  {
-    value: 'JPY',
-    label: '¥',
-  },
-];
+
+
 
 export default function NewBlog() {
   return (
     <Box
       component="form"
-      sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' },width:400 ,height:500, boxShadow: 'rgba(0, 0, 0, 0.87) 24px 12px 12px 12px',textAlign:"center",justifyContent:"center",display:"flex",alignItem:"center",m:"auto",mt:3,mb:3}}
+      sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' },width:400 ,height:500, boxShadow: 'rgba(0, 0, 0, 0.87) 24px 12px 12px 12px',textAlign:"center",justifyContent:"center",display:"flex",alignItem:"center",m:"auto",mt:3,mb:30}}
       noValidate
       autoComplete="off"
     >
       <div>
-        <TextField
-          id="outlined-select-currency"
-          select
-          label="Select"
-          defaultValue="EUR"
-          helperText="Please select your currency"
-        >
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+     
+      <TextField fullWidth label="Title*" id="fullWidth" />
+   
+      <TextField fullWidth label="Image URL*" id="fullWidth" />
+      <TextField select label="Category*" id="fullWidth"><MenuItem >Please Choose...
+             
+            </MenuItem></TextField>
+   
+      
         </div>
         </Box>
        ) }
